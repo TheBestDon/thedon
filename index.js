06f0e3fad5e5e1c15c7760c5dbd122b81dc0be50
TheBestDon <donatas_bakanas@yahoo.com>
2020-10-10 12:16:42 +0300

    👌 IMPROVE: Clears console

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+const clearConsole = require('clear-any-console')
+
+clearConsole()
+console.log(`
+Donatas Bakanas
+
+Software Engineer.
+
+🐦 @donas04
+↪ https://github.com/TheBestDon/
+`);
