a138ba638e7d6f9c250c7c21888d8fa1746c08c6
TheBestDon <donatas_bakanas@yahoo.com>
2023-06-23 11:11:30 +0300

    feat: :sparkles: code update

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 #!/usr/bin/env node
-const welcome = require('cli-welcome')
-const pkgJSON = require('./package.json')
-const chalk = require('chalk')
+import fs from 'node:fs/promises';
+import * as welcome from 'cli-welcome'
+import chalk from 'chalk'
 const log = console.log
 const dim = chalk.dim
 
+const packageJson = JSON.parse(await fs.readFile('package.json'));
 const social = color => text => chalk.hex(color).bold.inverse(text)
 
-welcome({
-  title: pkgJSON.name,
+welcome.default({
+  title: packageJson.name,
   tagLine: `Hey, nice to meet ya!`,
-  description: pkgJSON.description,
-  version: pkgJSON.version,
+  description: packageJson.description,
+  version: packageJson.version,
   bgColor: `#FADC00`,
   color: `#000000`,
   bold: true,
   clear: true
 })
 
-log(`${chalk.blue(' Donatas Bakanas ')}
+log(`${chalk.blue(` ${packageJson.author}`)}
 
 ${dim.italic(`Software Engineer.`)}
 
 🐦 ${social('#1da1f2')(` Twitter `)}: ${dim(`@donas04`)}
 ↪ ${social('#bada55')(` Github `)}: ${dim(`https://github.com/TheBestDon/`)}
+👔 ${social('#0077b5')(` LinkedIn `)}: ${dim(`https://www.linkedin.com/in/donatas-bakanas/`)}
 `);
